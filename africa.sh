700f9d89542b950cec5046970d70e5aff6730cfd
Chris Duncombe Rae <cmdrae.dea@gmail.com>
2016-08-16 15:06:57 +0200

    Updating .gitignore africa.sh

--- africa.sh
+++ africa.sh
@@ -3,12 +3,19 @@
 if hash gmt 2> /dev/null ; then
 	echo Running version 5 of GMT
 	GMTWRAP=gmt
-fi
-
-if hash GMT 2> /dev/null ; then
+elif hash GMT 2> /dev/null ; then
 	echo Running version 4 of GMT
 	GMTWRAP=""
+else
+	echo An executable version of GMT is apparently not found.
+	exit 1
 fi
 
-$GMTWRAP pscoast
+OUTPUTFILE=$(mktemp)
+
+$GMTWRAP pscoast -R-34/56/-40/41 -JM6i -W -P -B5 >> $OUTPUTFILE
+
+gv $OUTPUTFILE
+
+rm $OUTPUTFILE
 
