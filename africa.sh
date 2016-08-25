f5188007decf3873a04b3440cf602fe9d0a4ef24
Chris Duncombe Rae <cmdrae.dea@gmail.com>
2016-08-25 13:48:50 +0200

    Updating africa.sh gmtwrap

--- africa.sh
+++ africa.sh
@@ -1,19 +1,8 @@
 #! /bin/bash
 
-if hash gmt 2> /dev/null ; then
-	echo Running version 5 of GMT
-	GMTWRAP=gmt
-elif hash GMT 2> /dev/null ; then
-	echo Running version 4 of GMT
-	GMTWRAP=""
-else
-	echo An executable version of GMT is apparently not found.
-	exit 1
-fi
-
 OUTPUTFILE=$(mktemp)
 
-$GMTWRAP pscoast -R-34/56/-40/41 -JM6i -W -P -B5 >> $OUTPUTFILE
+gmtwrap pscoast -R-34/56/-40/41 -JM6i -W1/0 -Dc -P -B5 >> $OUTPUTFILE
 
 gv $OUTPUTFILE
 
